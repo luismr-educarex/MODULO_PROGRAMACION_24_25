4ba1aca5284aef056af954b32bd6d8f0539303cc
Luis Martinez <lmartinezr10@educarex.es>
2024-09-20 10:44:01 +0200

    Esto es otro cambio

--- src/unidades/unidad1/Programa1.java
+++ src/unidades/unidad1/Programa1.java
@@ -5,6 +5,7 @@ public class Programa1 {
 	public static void main(String[] args) {
 		System.out.println("1ºDAW");
 		System.out.println("Esto es una prueba de sincronización");
+		System.out.println("Esto es otro cambio");
 	}
 
 }
