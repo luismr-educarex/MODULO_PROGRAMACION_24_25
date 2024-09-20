4e86174da4c158021c3d742b9b5d683f3bd0efb1
luismr-educarex <luismr@educarex.es>
2024-09-20 19:17:48 +0200

    Se añade variables de tipo byte

--- src/unidades/unidad1/Programa2.java
+++ src/unidades/unidad1/Programa2.java
@@ -4,5 +4,11 @@ public class Programa2 {
 
     public static void main(String[] args) {
         System.out.println("Este es otro ejemplo");
+
+        byte numero = 100;
+        byte numero2 = 127;
+
+        System.out.println(numero);
+        System.out.println(numero2);
     }
 }
