ac97db174f200226a8844c8ce359fc5003bcc51f
profesor <profesor@equipo-00>
2024-10-03 08:48:51 +0200

    quito código

--- src/unidades/unidad1/Programa2.java
+++ src/unidades/unidad1/Programa2.java
@@ -3,13 +3,6 @@ package unidades.unidad1;
 public class Programa2 {
 
     public static void main(String[] args) {
-        System.out.println("Este es otro ejemplo");
-
-        byte numero = 100;
-        byte numero2 = 127;
-        int numero3 = 12000;
-
-        System.out.println(numero);
-        System.out.println(numero2);
+       
     }
 }
