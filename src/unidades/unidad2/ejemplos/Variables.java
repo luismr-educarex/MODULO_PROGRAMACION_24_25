f50c11b8ef177d84a057df0c375113a18d0d7641
profesor <profesor@equipo-00>
2024-10-03 10:28:54 +0200

    ejemplos variables

---
+++ src/unidades/unidad2/ejemplos/Variables.java
@@ -0,0 +1,25 @@
+package unidades.unidad2.ejemplos;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		
+				//ejemplos de declaración variables de tipo entero
+				int cantidadCochesCasa;
+				int cantidadCochesTrabajo;
+				int sumaCoches;
+		
+				cantidadCochesCasa=4;
+				cantidadCochesTrabajo=3;
+				sumaCoches=0;
+				
+				sumaCoches = cantidadCochesCasa+cantidadCochesTrabajo;
+					
+				System.out.println("Suma total:"+sumaCoches);
+			
+				
+		
+
+	}
+
+}
