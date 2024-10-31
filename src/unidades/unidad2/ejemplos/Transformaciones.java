52372a7033f16a95ca83528c671dd109094fcc8e
luis-educarex <lmartinezr10@educarex.es>
2024-10-31 11:15:39 +0100

    Ejemplos jueves

---
+++ src/unidades/unidad2/ejemplos/Transformaciones.java
@@ -0,0 +1,17 @@
+package unidades.unidad2.ejemplos;
+
+public class Transformaciones {
+
+    public static void main(String[] args) {
+
+        double media = 234.56;
+
+        int datoFinal;
+
+        datoFinal  = (int)media;
+
+        System.out.println("Dato final:"+datoFinal);
+
+
+    }
+}
