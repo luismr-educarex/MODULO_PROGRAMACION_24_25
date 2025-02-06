8b1b476cc9007fd9364bcc2b5efb2faac7fdd913
luis-educarex <lmartinezr10@educarex.es>
2025-02-06 11:15:44 +0100

    Cositas del jueves sobre expresiones regulares

--- src/unidades/unidad7/ejemplos/expresionesregulares/Main.java
+++ src/unidades/unidad7/ejemplos/expresionesregulares/Main.java
@@ -6,9 +6,53 @@ import java.util.regex.Pattern;
 public class Main {
 
 	public static void main(String[] args) {
+		
+		String texto = "Hoy es 06/02/2025 y mañana será "
+				+ "07/02/2025";
+		
+		String patron = "(\\d{2})/(\\d{2})/(\\d{4})";
+		
+		Pattern patternFechas = Pattern.compile(patron);
+		
+		Matcher matcherFechas = patternFechas.matcher(texto);
+		
+		
+		while (matcherFechas.find()) {
+			System.out.println("Fecha completa:"+matcherFechas.group(0));
+			System.out.println("Dia:"+matcherFechas.group(1));
+			System.out.println("Mes:"+matcherFechas.group(2));
+			System.out.println("Año"+matcherFechas.group(3));
+			System.out.println("-------------------");
+		}
+		
+		
+		
+		
+		
+		String dni = "45678932-J";
+		
+
+		
+		String []datosDni = dni.toUpperCase().split("[A-Z]|-");
+
+		
+		for (String dato : datosDni) {
+			System.out.println(dato);
+		}
+		
 	
-		String cadena = "91";
+		String cadena = "12-7/2012";
+		
+		String []datos = cadena.split("[/-]");
+		
+
+		
+		for (String dato : datos) {
+			System.out.println(dato);
+		}
+		
 		Pattern pattern =  Pattern.compile("[159]");
+		
 		Matcher matcher = pattern.matcher(cadena);
 
 		if(matcher.matches()) {
@@ -17,6 +61,7 @@ public class Main {
 			System.out.println("No encontrado");
 		}
 		
+
 		String cadena2 = "1Cic5los9";
 		Pattern pattern2 =  Pattern.compile("[951]");
 		Matcher matcher2 = pattern2.matcher(cadena2);
