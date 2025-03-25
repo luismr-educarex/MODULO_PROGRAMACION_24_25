4504ee0d238a8264a8ddf54522d80780d83f6fd4
luismr-educarex <luismr@educarex.es>
2025-03-25 13:24:35 +0100

    Ejemplos unidad 10 Excepciones

---
+++ src/unidades/unidad10/ejemplos/sumas/SumaArgumentos3.java
@@ -0,0 +1,34 @@
+package unidades.unidad10.ejemplos.sumas;
+
+public class SumaArgumentos3 {
+
+	public static void main(String[] args) {
+		
+		int suma=0;
+		
+		for(int i=0;i<args.length;i++) {
+		
+			try {
+				suma += Integer.parseInt(args[i]);
+			}
+
+			catch(NumberFormatException e){
+				System.out.println("Uno de los argumentos "
+						+ "recibidos no es un entero");
+				System.err.println("Argumento inválido"+args[i]);
+			}
+			catch(Exception e){
+				System.out.println("Se ha producido un error");
+			}
+		
+		}// !!!!! fin del for !!!!
+		
+		System.out.println("Suma:"+suma);
+		
+
+	}
+
+}
+
+
+
